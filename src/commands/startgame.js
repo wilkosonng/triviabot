a05df2b950d37bbcd208078a34be6a756c852b98
Wilson Kong <wilkosonng@gmail.com>
2023-06-20 18:09:10 -0400

    Switched from depricated string-similarity package to string-similarity-js

--- src/commands/startgame.js
+++ src/commands/startgame.js
@@ -1,8 +1,8 @@
-const { SlashCommandBuilder, Events, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { teams, teamEmojis } = require('../../config.json');
 const { initializeApp } = require('firebase/app');
 const { getDatabase, ref, get } = require('firebase/database');
-const { playGame } = require('../game/playGame');
+const { playGame } = require('../game/playgame');
 require('dotenv').config();
 
 const firebaseApp = initializeApp(JSON.parse(process.env.FIREBASE_CREDS));
@@ -40,7 +40,7 @@ module.exports = {
 	async execute(interaction) {
 		await interaction.deferReply();
 
-		if (currGames.has(interaction.channel)) {
+		if (currGames.has(interaction.channel.id)) {
 			await interaction.editReply('Error: Game has already started in this channel!');
 			return;
 		}
@@ -50,15 +50,15 @@ module.exports = {
 		const losePoints = interaction.options?.getBoolean('losepoints') ?? true;
 		const shuffle = interaction.options?.getBoolean('shuffle') ?? true;
 		const channel = interaction.channel;
-		const host = interaction.user;
 		const teamInfo = new Map();
 		const players = new Map();
 		let questions, description, collector;
 		let titleExists = false;
 
-		currGames.add(channel);
+		currGames.add(channel.id);
 		for (let i = 0; i < numTeams; i++) {
 			teamInfo.set(teamEmojis[i], {
+				name: teams[i],
 				players: new Set(),
 				score: 0
 			});
@@ -91,7 +91,7 @@ module.exports = {
 				}
 			});
 		} catch (error) {
-			console.log(error);
+			console.error(error);
 			return interaction.editReply({
 				content: 'Database reference error.',
 			});
@@ -117,42 +117,42 @@ module.exports = {
 			// Ah yes, socket timeout.
 			await channel.send({
 				embeds: [generateStartEmbed()]
-			}).then(async (message) => {
-				for (let i = 0; i < numTeams; i++) {
-					await message.react(message.client.emojis.cache.get(teamEmojis[i]));
-				}
-
-				collector = message.createReactionCollector({
-					filter: (reaction, user) => !user.bot && teamEmojis.includes(reaction.emoji.id),
-					time: 120_000
-				});
-
-				collector.on('collect', (reaction, user) => {
-					const newTeam = reaction.emoji.id;
-					const player = user.id;
-
-					if (players.has(player)) {
-						const oldTeam = players.get(player)['team'];
-						if (oldTeam === newTeam) {
-							return;
-						}
-						teamInfo.get(oldTeam).players.delete(player);
+			})
+				.then(async (msg) => {
+					for (let i = 0; i < numTeams; i++) {
+						await msg.react(msg.client.emojis.cache.get(teamEmojis[i]));
 					}
 
-					teamInfo.get(newTeam).players.add(player);
-					players.set(player, {
-						team: newTeam,
-						score: 0
+					collector = msg.createReactionCollector({
+						filter: (reaction, user) => !user.bot && teamEmojis.includes(reaction.emoji.id),
+						time: 120_000
 					});
 
-					message.edit(
-						{
-							embeds: [generateStartEmbed()]
+					collector.on('collect', (reaction, user) => {
+						const newTeam = reaction.emoji.id;
+						const player = user.id;
+
+						if (players.has(player)) {
+							const oldTeam = players.get(player)['team'];
+							if (oldTeam === newTeam) {
+								return;
+							}
+							teamInfo.get(oldTeam).players.delete(player);
 						}
-					);
-					channel.send(`${user.tag} has joined the ${teams[teamEmojis.indexOf(reaction.emoji.id)]} team!`);
+
+						teamInfo.get(newTeam).players.add(player);
+						players.set(player, {
+							team: newTeam,
+							score: 0
+						});
+
+						msg.edit(
+							{
+								embeds: [generateStartEmbed()]
+							}
+						);
+					});
 				});
-			});
 
 		} catch (error) {
 			console.error(error);
@@ -160,27 +160,30 @@ module.exports = {
 		}
 
 		const startCollector = channel.createMessageCollector({
-			filter: (msg) => !msg.author.bot && (msg.content.toLowerCase() === 'endtrivia' || (msg.content.toLowerCase() === 'ready' && msg.author.id === host)),
+			filter: (msg) => msg.author?.id === interaction.user.id && (msg.content.toLowerCase() === 'endtrivia' || (msg.content.toLowerCase() === 'ready')),
 			time: 180_000
 		});
 
-		startCollector.on('collect', (msg) => {
+		startCollector.on('collect', async (msg) => {
 			const lowercaseMsg = msg.content.toLowerCase();
 			switch (lowercaseMsg) {
 				case 'ready': {
 					startCollector.stop();
 					collector.stop();
-					msg.reply('Game starting...');
-					playGame(channel, teamInfo, players, losePoints, set, questions, interaction.client);
+					msg.reply('Game starting... Type `endtrivia` to end the game, `playerlb` to access player scores, `teamlb` to access team scores, and `buzz` to buzz in for a question!');
+					await playGame(channel, teamInfo, players, losePoints, set, questions, interaction.client);
+					currGames.delete(channel.id);
+					break;
 				};
 				case 'endtrivia': {
 					endGame();
 					msg.reply('Game ended');
+					break;
 				};
 			}
 		});
 
-		startCollector.on('end', (collected, reason) => {
+		startCollector.on('end', (_, reason) => {
 			switch (reason) {
 				case 'time': {
 					channel.send('Game timed out');
@@ -220,10 +223,11 @@ module.exports = {
 			return msg;
 		}
 
+		// Thanos time
 		function endGame() {
 			startCollector.stop();
 			collector.stop();
-			currGames.delete(channel);
+			currGames.delete(channel.id);
 		}
 
 		// Defines a shuffle algorithm to randomize questions.
--- src/game/playgame.js
+++ src/game/playgame.js
@@ -1,22 +1,200 @@
-const { threshold, teams, teamEmojis } = require('../../config.json');
+const { threshold } = require('../../config.json');
 const similarity = require('string-similarity');
-const { EmbedBuilder, Events } = require('discord.js');
-
-let gameCollector;
+const { EmbedBuilder, MessageCollector } = require('discord.js');
 
 // Starts the game passed through.
 
 async function playGame(channel, teamInfo, players, losePoints, set, questions, client) {
+	let questionNumber = 1;
+	let ended = false;
+
 	const commandCollector = channel.createMessageCollector({
 		filter: (msg) => {
 			const content = msg.content.toLowerCase();
-			return content === 'endtrivia' || content === 'leaderboards';
+			return content === 'endtrivia' || content === 'teamlb' || content === 'playerlb';
 		}
 	});
+
+	commandCollector.on('collect', (msg => {
+		const command = msg.content.toLowerCase();
+		switch (command) {
+			case 'endtrivia': {
+				endGame();
+				break;
+			}
+			case 'teamlb': {
+				channel.send({
+					embeds: [generatePlayerEmbed(teamInfo)]
+				});
+				break;
+			}
+			case 'playerlb': {
+				channel.send({
+					embeds: [generateTeamEmbed(players)]
+				});
+				break;
+			}
+		}
+	}));
+
+	while (questions.length && !ended) {
+		const nextQuestion = questions.shift();
+
+		await channel.send(
+			{
+				embeds: [generateQuestionEmbed(set, questionNumber, nextQuestion)]
+			})
+			.then((msg) => {
+				channel.awaitMessages({
+					filter: (m) => players.has(m.author.id) && m.content.toLowerCase() === 'buzz',
+					max: 1,
+					time: 20_000
+				})
+					.then((async (buzz) => {
+						const answerer = buzz.author.id;
+						const answerTeam = teamInfo.get(players.get(answerer).team);
+						msg.edit({
+							embeds: [msg.embeds[0].setDescription('_ _')]
+						});
+
+						await channel.send({
+							embeds: [generateBuzzEmbed(buzz.author.displayName, answerTeam, client)]
+						});
+
+						channel.awaitMessages({
+							filter: (m) => m.author.id === buzz.author.id,
+							max: 1,
+							time: 10_000
+						})
+							.then((ans) => {
+								if (judgeAnswer(question, ans.content)) {
+									channel.send({
+										embeds: [generateResultEmbed('correct', nextQuestion, losePoints, buzz.author.displayName, ans.content)]
+									});
+								} else {
+									channel.send({
+										embeds: [generateResultEmbed('incorrect', nextQuestion, losePoints, buzz.author.displayName, ans.content)]
+									});
+								}
+							})
+							.catch(() => {
+								channel.send({
+									embeds: [generateResultEmbed('time', nextQuestion, losePoints, buzz.author.displayName)]
+								});
+							});
+					}))
+					.catch(() => {
+						awaitchannel.send({
+							embeds: [generateResultEmbed('nobuzz', nextQuestion, losePoints, null)]
+						});
+					});
+			});
+	}
+
+	if (!ended) {
+		endGame();
+	}
+
+	channel.send({
+		embeds: [generateTeamEmbed(), generatePlayerEmbed()]
+	});
+
+	function endGame() {
+		ended = true;
+		commandCollector.stop();
+	}
+}
+
+function generateTeamEmbed() {
+	const msg = new EmbedBuilder()
+		.setColor(0xD1576D)
+		.setTitle('🏆 Team Standings 🏆')
+		.setDescription(description);
+
+	return msg;
+}
+
+function generatePlayerEmbed() {
+	const msg = new EmbedBuilder()
+		.setColor(0xD1576D)
+		.setTitle('🏆 Player Standings 🏆')
+		.setDescription(description);
+
+	return msg;
+}
+
+function generateQuestionEmbed(set, num, question) {
+	const msg = new EmbedBuilder()
+		.setColor(0xD1576D)
+		.setTitle(`❔ ${set} ※ Question ${num} ❔`)
+		.setDescription(question?.question ?? '_ _');
+
+	return msg;
+}
+
+function generateBuzzEmbed(playerName, team, client) {
+	const emoji = client.emojis.cache.get(team);
+	const msg = new EmbedBuilder()
+		.setColor(0xD1576D)
+		.setTitle(`${emoji} ${playerName} has buzzed in! ${emoji}`)
+		.setDescription('You have 10 seconds to answer!');
+
+	return msg;
+}
+
+function generataResultEmbed(correct, question, losePoints, answerer, response) {
+	let emoji, message, description;
+	switch (correct) {
+		case 'correct': {
+			emoji = '✅';
+			message = 'Correct!';
+			description = `${answerer} has just scored themselves and their team 1 point!`;
+		}
+		case 'incorrect': {
+			emoji = '❌';
+			message = 'Incorrect';
+			description = losePoints ? `${answerer} has just lost their team 1 point!` : `Unfortunately, ${answerer} did not answer correctly!`;
+		}
+		case 'time': {
+			emoji = '⏱️';
+			message = 'Time\'s Up!';
+			description = losePoints ? `${answerer} has just lost their team 1 point!` : `Unfortunately, ${answerer} did not answer correctly!`;
+		}
+		case 'nobuzz': {
+			emoji = '😭';
+			message = 'No takers?';
+			description = 'Cold feet, eh? No change in the standings.';
+		}
+	}
+	const msg = new EmbedBuilder()
+		.setColor(0xD1576D)
+		.setTitle(`${emoji} ${message} ${emoji}`)
+		.setDescription(description)
+		.addFields(
+			{
+				name: 'Question',
+				value: question.question
+			},
+			{
+				name: 'Answer',
+				value: question.answer.join(', ')
+			});
+
+	if (response) {
+		msg.addFields({
+			name: 'Player Response',
+			value: response
+		});
+	}
+
+	return msg;
+}
+
+function judgeAnswer(question, answer) {
+	return string-similary
 }
 
 // Defines a tolerance for how similar a submission must be to an answer to be "correct"
-
 function answerThreshold(str) {
 	return 0.95 * Math.pow(Math.E, -(threshold / str.length()));
 }
